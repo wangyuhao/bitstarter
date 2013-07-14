4690244b3a4cd3ad6717eff028158d364c7ddbc9
yuhao wang <yuhao@yuhao-Satellite-A305.(none)>
2013-07-14 12:56:17 -0400

    edit web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync("idnex.html");
+  var buffer = fs.readFileSync("index.html");
   response.send(buffer.toString());
 });
 
