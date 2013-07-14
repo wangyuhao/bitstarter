a6e0f2028eb9211497d8ff4a539ed6c6f85c2046
yuhao wang <yuhao@yuhao-Satellite-A305.(none)>
2013-07-14 12:52:34 -0400

    modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("idnex.html");
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
